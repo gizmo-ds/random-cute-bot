d380d3b56a6646948a8860812ef21ba9d1aa0051
Gizmo <cunyu@liuli.lol>
2022-04-27 01:06:43 +0800

    chore: update sift

--- random-cute.ts
+++ random-cute.ts
@@ -2,7 +2,7 @@ import {
   json,
   serve,
   validateRequest,
-} from "https://deno.land/x/sift@0.4.0/mod.ts";
+} from "https://deno.land/x/sift@0.5.0/mod.ts";
 import { cuteData, DiscordRequest, verifySignature } from "./discord.ts";
 import { cutes } from "./cutes.ts";
 
